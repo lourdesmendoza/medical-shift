ad337849b6d1f176e5bc228a0f22839ab549ef07
Lourdes Mendoza <lmendoza@teco.com.ar>
2020-06-02 01:32:54 -0300

    Added form and states

---
+++ src/components/Formulario.js
@@ -0,0 +1,84 @@
+import React, { Fragment, useState } from 'react';
+
+const Formulario = () => {
+    // Create appointment state
+    const [appointment, setAppointment] = useState({
+        pet: '',
+        owner: '',
+        date: '',
+        hour: '',
+        symptoms: ''
+    }) 
+
+    // Function that executes when the user writes in the form
+    const handleChange = e => {
+        setAppointment({
+            ...appointment,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Get values
+    const {pet, owner, date, hour, symptoms } = appointment;
+
+    return (
+        <Fragment>
+            <h2>Create appointment</h2>
+
+            <form>
+                <label>Pet's name</label>
+                <input
+                    type="text"
+                    name="pet"
+                    className="u-full-width"
+                    placeholder="Name of your pet"
+                    onChange={handleChange}
+                    value={pet}
+                />
+
+                <label>Owner's name</label>
+                <input
+                    type="text"
+                    name="owner"
+                    className="u-full-width"
+                    placeholder="Owner's name"
+                    onChange={handleChange}
+                    value={owner}
+                />
+
+                <label>Date</label>
+                <input
+                    type="date"
+                    name="date"
+                    className="u-full-width"
+                    onChange={handleChange}
+                    value={date}
+                />
+
+                <label>Hour</label>
+                <input
+                    type="time"
+                    name="hour"
+                    className="u-full-width"
+                    onChange={handleChange}
+                    value={hour}
+                />
+
+                <label>Symptoms</label>
+                <textarea
+                    className="u-full-width"
+                    name="symptoms"
+                    onChange={handleChange}
+                    value={symptoms}
+                ></textarea>
+
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Add appointment</button>
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default Formulario;
